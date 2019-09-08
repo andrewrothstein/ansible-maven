4f1f0e3300efbd10bff0b1058bd26c77683592cc
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-09-08 13:01:49 -0500

    up to v3.6.2. upgrade dep role versions.

---
+++ dl-checksum.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+MAJOR_VER=3
+VER=${MAJOR_VER}.6.2
+DIR=~/Downloads
+SHATYPE=512
+URL=http://archive.apache.org/dist/maven/maven-${MAJOR_VER}/${VER}/binaries/apache-maven-${VER}-bin.tar.gz.sha${SHATYPE}
+printf "  # %s\n" $URL
+printf "  '%s': sha%s:%s\n" $VER $SHATYPE $(curl -sSL $URL | awk '{print $1}')
