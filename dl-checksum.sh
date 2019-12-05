e433f5ee239072c3e9bda32b2c0971d8445ffa5a
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-12-05 00:04:44 -0500

    up to v3.6.3

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 MAJOR_VER=3
-VER=${MAJOR_VER}.6.2
+VER=${MAJOR_VER}.6.3
 DIR=~/Downloads
 SHATYPE=512
 URL=http://archive.apache.org/dist/maven/maven-${MAJOR_VER}/${VER}/binaries/apache-maven-${VER}-bin.tar.gz.sha${SHATYPE}
